f6e4fafd27b71f5d12ed01b06db4e1d9cde70197
gwegwe1234 <gwangtae5860@daum.net>
2020-02-12 08:28:14 +0900

    refactoring

--- HackerRank/src/FormingMagicSquare.java
+++ HackerRank/src/FormingMagicSquare.java
@@ -1,10 +1,4 @@
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
 
 public class FormingMagicSquare {
