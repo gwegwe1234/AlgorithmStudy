494f327230cef37bebf0355e1c2958df8230a7c1
gwegwe1234 <gwangtae5860@daum.net>
2020-12-05 18:02:35 +0900

    Code Plus 알고리즘 기초 2 ~ 비트마스크

---
+++ CodePlusAlgo/src/basic2/SumOfSubSet.java
@@ -0,0 +1,33 @@
+package basic2;
+
+import java.util.Scanner;
+
+public class SumOfSubSet {
+
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int s = sc.nextInt();
+    int[] a = new int[n];
+    for (int i=0; i<n; i++) {
+      a[i] = sc.nextInt();
+    }
+
+    int ans = 0;
+
+    for (int i = 1; i < (1<<n); i++) {
+      int sum = 0;
+      for (int k = 0; k < n; k++) {
+        if ((i & (1<<k)) != 0) {
+          sum += a[k];
+        }
+      }
+
+      if (sum == s) {
+        ans++;
+      }
+    }
+
+    System.out.println(ans);
+  }
+}
